63fce64f10474b2dafe95c8c96a4cc4ef341eefb
aa13.a.klints@pt.edu.lv <aa13.a.klints@pt.edu.lv>
2017-09-27 14:08:49 +0300

    20170927_14_10

---
+++ shell_examples.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#3. piemērs
+NAME="Vārds Uzvārds"
+echo $NAME
+unset NAME
+echo $NAME
+
+#2. piemērs
+#NAME="Vārds Uzvārds"
+#readonly NAME
+#echo $NAME
+#NAME="Vārds Uzvārds"
+#echo $NAME
+
+
+
+#1. piemērs
+#NAME="Vārds Uzvārds"
+#echo $NAME
+
+history > history_20170927.txt
+
