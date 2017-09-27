32967a80cb0eab82248b958596f57b83f0aff9f7
aa13.a.klints@pt.edu.lv <aa13.a.klints@pt.edu.lv>
2017-09-27 16:08:58 +0300

    20170927_16_10

--- shell_examples.sh
+++ shell_examples.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
 
+#7. piemeers
+#skaitliskaa_veertiiba='expr 2 + 2'
+#echo "Summas vērtība: "$skaitliskaa_veertiiba
+#echo "Summas vērtība: $skaitliskaa_veertiiba"
+skaitliskaa_veertiiba=`expr 2 + 2`
+echo "Summas vērtība: "$skaitliskaa_veertiiba
+echo "Summas vērtība: $skaitliskaa_veertiiba"
+#skaitliskaa_veertiiba=expr 2 + 2
+#echo "Summas vērtība: "$skaitliskaa_veertiiba
+#echo "Summas vērtība: $skaitliskaa_veertiiba"
+
+#6. piemērs
+#echo $*
+#echo "--------"
+#kaartas_numurs=1
+#for arguments in $*
+#do
+#    echo $kaartas_numurs". arguments - "$arguments
+#    kaartas_numurs=$kaartas_numurs+1
+#done
+
+#5. piemers
+#echo "Skriptam nodoto argumentu skaits: " $#
+#echo "Argumentu saraksts (attēlošana/grupēšana veids 1): "$*
+#echo "Argumentu saraksts (attēlošana/grupēšana veids 1): "$@
+#echo "Pirmā argumenta vērtība: "$1
+#echo "Otrā argumenta vērtība: "$2
+#echo $1$2
+
+#4. piemers
+#echo $!
+
 #3. piemērs
-NAME="Vārds Uzvārds"
-echo $NAME
-unset NAME
-echo $NAME
+#NAME="Vārds Uzvārds"
+#echo $NAME
+#unset NAME
+#echo $NAME
 
 #2. piemērs
 #NAME="Vārds Uzvārds"
