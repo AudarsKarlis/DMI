9c23a7355ce16e18231a6afe4df4736bb8951b52
aa13.a.klints@pt.edu.lv <aa13.a.klints@pt.edu.lv>
2017-10-18 16:08:29 +0300

    20171018_16_10

---
+++ a_VS_b_VS_c.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+echo "Input a:"
+read a
+echo "Input b:"
+read b
+echo "Input c:"
+read c
+
+#if [ $a -gt $b ]
+if (( $a > $b ))
+then
+echo "a ($a) > b ($b)"
+#elif [ $a -lt $b ]
+elif (( $a < $b ))
+then
+echo "a ($a) < b ($b)"
+else
+echo "a ($a) = b ($b)"
+fi
+
+: <<'END'
+echo "Input a:"
+read a
+echo "Input b:"
+read b
+if [ $a -gt $b ]
+then
+echo "jā, a ($a) > b ($b)"
+else
+echo "nē, a ($a) nav lielāks par b ($b)"
+fi
+END
+
+
+
+: <<'END'
+c=`expr $a + $b`
+echo "$a + $b = "$c
+END
